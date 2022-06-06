8bcf4ee77a23160648c562834b5e99b1325ac7c7
Gonzalo Arca <goarca@itba.edu.ar>
2022-06-05 21:01:46 -0300

    :sparkles: add ej c for infection_rate

--- post-processing/utils/Constants.py
+++ post-processing/utils/Constants.py
@@ -10,4 +10,5 @@ class Constants(enum.Enum):
     STATIC_FILE_NAME = "static.txt"
     ROOM_FILE_NAME = "room.xyz"
     ANIMATION_FILE_NAME = "epidemic.xyz"
-    INFECTION_RATE_FILE_NAME = "ej_b_infection_rate.csv"
+    INFECTION_RATE_FILE_NAME_NH = "ej_b_infection_rate.csv"
+    INFECTION_RATE_FILE_NAME_VDZ = "ej_c_infection_rate.csv"
